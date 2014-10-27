a16fc7167358610045aa5cde887f49556d273107
Mike Land <mikepland@gmail.com>
2014-10-26 19:55:12 -0500

    sets up AdjectiveViewController

---
+++ wordPlay2MM/EnterAdjectiveViewController.h
@@ -0,0 +1,13 @@
+//
+//  EnterAdjectiveViewController.h
+//  wordPlay2MM
+//
+//  Created by MIKE LAND on 10/26/14.
+//  Copyright (c) 2014 MIKE LAND. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface EnterAdjectiveViewController : UIViewController
+
+@end
---
+++ wordPlay2MM/EnterAdjectiveViewController.m
@@ -0,0 +1,38 @@
+//
+//  EnterAdjectiveViewController.m
+//  wordPlay2MM
+//
+//  Created by MIKE LAND on 10/26/14.
+//  Copyright (c) 2014 MIKE LAND. All rights reserved.
+//
+
+#import "EnterAdjectiveViewController.h"
+
+@interface EnterAdjectiveViewController ()
+@property (weak, nonatomic) IBOutlet UITextField *adjectiveTextField;
+
+@end
+
+@implementation EnterAdjectiveViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
