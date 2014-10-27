9df282bd43edce369a76bc0f02c8f625c9287617
Mike Land <mikepland@gmail.com>
2014-10-26 19:50:12 -0500

    sets up EnterNameViewController

---
+++ wordPlay2MM/EnterNameViewController.h
@@ -0,0 +1,13 @@
+//
+//  EnterNameViewController.h
+//  wordPlay2MM
+//
+//  Created by MIKE LAND on 10/26/14.
+//  Copyright (c) 2014 MIKE LAND. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface EnterNameViewController : UIViewController
+
+@end
