6fb3f900a210e94e11b9670a7aa367214981b773
Mike Land <mikepland@gmail.com>
2014-10-26 19:29:27 -0500

    Initial Commit

---
+++ wordPlay2MM/AppDelegate.h
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.h
+//  wordPlay2MM
+//
+//  Created by MIKE LAND on 10/26/14.
+//  Copyright (c) 2014 MIKE LAND. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+
+@end
+
---
+++ wordPlay2MMTests/wordPlay2MMTests.m
@@ -0,0 +1,40 @@
+//
+//  wordPlay2MMTests.m
+//  wordPlay2MMTests
+//
+//  Created by MIKE LAND on 10/26/14.
+//  Copyright (c) 2014 MIKE LAND. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface wordPlay2MMTests : XCTestCase
+
+@end
+
+@implementation wordPlay2MMTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
