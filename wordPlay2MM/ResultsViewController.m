a93e4bc8fec181b98bf9347a966eb9dc1b57f655
Mike Land <mikepland@gmail.com>
2014-10-26 21:32:28 -0500

    adds results viewcontroller

---
+++ wordPlay2MM/ResultsViewController.m
@@ -0,0 +1,38 @@
+//
+//  ResultsViewController.m
+//  wordPlay2MM
+//
+//  Created by MIKE LAND on 10/26/14.
+//  Copyright (c) 2014 MIKE LAND. All rights reserved.
+//
+
+#import "ResultsViewController.h"
+
+@interface ResultsViewController ()
+@property (weak, nonatomic) IBOutlet UITextField *resultsTextView;
+
+@end
+
+@implementation ResultsViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
